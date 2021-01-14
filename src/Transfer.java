a2916e25c884ebaa7e694f726fcc1c8b775921ac
TS <59762013+TsurukawaSalt@users.noreply.github.com>
2021-01-14 22:43:29 +0800

    optimize

--- src/Transfer.java
+++ src/Transfer.java
@@ -3,138 +3,141 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Transfer {
-    private List<Global> globals;
+    private List<Global> globalTable;
     private FunctionDef start;
-    private List<FunctionDef> functionDefs;
+    private List<FunctionDef> functionDefTable;
     private List<Byte> output;
 
     int magic = 0x72303b3e;
     int version = 0x00000001;
 
-    public Transfer(List<Global> globals, FunctionDef start, List<FunctionDef> functionDefs) throws FileNotFoundException {
-        this.globals = globals;
+    public Transfer(List<Global> globalTable, FunctionDef start, List<FunctionDef> functionDefTable) throws FileNotFoundException {
+        this.globalTable = globalTable;
         this.start = start;
-        this.functionDefs = functionDefs;
+        this.functionDefTable = functionDefTable;
         output = new ArrayList<>();
     }
 
     public List<Byte> generate() throws IOException{
-        //magic
-        List<Byte> magic=int2bytes(4,this.magic);
-        output.addAll(magic);
-        //version
-        List<Byte> version=int2bytes(4,this.version);
-        output.addAll(version);
+        // magic
+        transAddInt(4, magic);
 
-        //globals.count
-        List<Byte> globalCount = int2bytes(4, globals.size());
-        output.addAll(globalCount);
+        // version
+        transAddInt(4, version);
 
-        for (Global global: globals){
-            // isConstant
-            List<Byte> isConst = int2bytes(1, global.getIs_const());
-            output.addAll(isConst);
+        // globals.count
+        transAddInt(4, globalTable.size());
 
+        for (Global global: globalTable){
+            // is_const
+            transAddInt(1, global.getIs_const());
             // value.count
-            List<Byte> globalValueCount;
-
-            // value items
-            List<Byte> globalValue;
+            // value.items
             if (global.getValueItems() == null){
-                globalValueCount = int2bytes(4, 8);
-                globalValue = long2bytes(8,0L);
+                transAddInt(4, 8);
+                transAddLong(8, 0L);
             } else {
-                globalValue = String2bytes(global.getValueItems());
-                globalValueCount = int2bytes(4, globalValue.size());
+                transAddInt(4, global.getValueItems().length());
+                transAddString(global.getValueItems());
             }
-
-            output.addAll(globalValueCount);
-            output.addAll(globalValue);
         }
 
-        // function.count
-        List<Byte> functionsCount = int2bytes(4, functionDefs.size() + 1);
-        output.addAll(functionsCount);
+        // functions.count
+        transAddInt(4, functionDefTable.size() + 1);
 
+        // start
         generateFunction(start);
 
-        for (FunctionDef functionDef: functionDefs){
+        // other functions
+        for (FunctionDef functionDef: functionDefTable){
             generateFunction(functionDef);
         }
+
         return output;
     }
 
     private void generateFunction(FunctionDef functionDef) throws IOException{
         // name
-        List<Byte> name = int2bytes(4, functionDef.getNameIndex());
-        output.addAll(name);
+        transAddInt(4, functionDef.getNameIndex());
 
-        //retSlots
-        List<Byte> retSlots = int2bytes(4,functionDef.getReturnSlots());
-        output.addAll(retSlots);
+        // ret_slots
+        transAddInt(4, functionDef.getReturnSlots());
 
-        //paramsSlots;
-        List<Byte> paramsSlots=int2bytes(4,functionDef.getParamSlots());
-        output.addAll(paramsSlots);
+        // params_slots;
+        transAddInt(4, functionDef.getParamSlots());
 
-        //locSlots;
-        List<Byte> locSlots=int2bytes(4,functionDef.getLocSlots());
-        output.addAll(locSlots);
+        // loc_slots;
+        transAddInt(4, functionDef.getLocSlots());
 
+        // body.count
+        transAddInt(4, functionDef.getBody().size());
+
+        // body.items
         List<Instruction> instructions = functionDef.getBody();
-
-        //bodyCount
-        List<Byte> bodyCount=int2bytes(4, instructions.size());
-        output.addAll(bodyCount);
-
-        //instructions
         for(Instruction instruction : instructions){
-            //type
-            List<Byte> type = int2bytes(1, instruction.getInsType().getByteVal());
-            output.addAll(type);
-            //out.writeBytes(type.toString());
-
+            transAddInt(1, instruction.getInsType().getByteVal());
             if(instruction.getParam() != null){
-                List<Byte>  x;
-                if(instruction.getInsType() == InstructionType.Push)
-                    x = long2bytes(8,instruction.getParam());
-                else
-                    x = int2bytes(4,instruction.getParam().intValue());
-                output.addAll(x);
+                if(instruction.getInsType() == InstructionType.Push){
+                    transAddLong(8, instruction.getParam());
+                }
+                else{
+                    transAddInt(4,instruction.getParam().intValue());
+                }
             }
         }
     }
 
-    private List<Byte> Char2bytes(char value) {
-        List<Byte>  AB=new ArrayList<>();
-        AB.add((byte)(value&0xff));
-        return AB;
-    }
-
-    private List<Byte> String2bytes(String valueString) {
-        List<Byte>  AB=new ArrayList<>();
-        for (int i=0;i<valueString.length();i++){
-            char ch=valueString.charAt(i);
-            AB.add((byte)(ch&0xff));
+//    private List<Byte> Char2bytes(char value) {
+//        List<Byte>  AB=new ArrayList<>();
+//        AB.add((byte)(value&0xff));
+//        return AB;
+//    }
+//
+//    private List<Byte> String2bytes(String valueString) {
+//        List<Byte>  AB=new ArrayList<>();
+//        for (int i=0;i<valueString.length();i++){
+//            char ch=valueString.charAt(i);
+//            AB.add((byte)(ch&0xff));
+//        }
+//        return AB;
+//    }
+//
+//    private List<Byte> long2bytes(int length, long target) {
+//        ArrayList<Byte> bytes = new ArrayList<>();
+//        int start = 8 * (length-1);
+//        for(int i = 0 ; i < length; i++){
+//            bytes.add((byte) (( target >> ( start - i * 8 )) & 0xFF ));
+//        }
+//        return bytes;
+//    }
+//
+//    private ArrayList<Byte> int2bytes(int length,int target){
+//        ArrayList<Byte> bytes = new ArrayList<>();
+//        int start = 8 * (length-1);
+//        for(int i = 0 ; i < length; i++){
+//            bytes.add((byte) (( target >> ( start - i * 8 )) & 0xFF ));
+//        }
+//        return bytes;
+//    }
+    
+    private void transAddString(String s){
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            output.add((byte)c);
         }
-        return AB;
     }
 
-    private List<Byte> long2bytes(int length, long target) {
-        ArrayList<Byte> bytes = new ArrayList<>();
+    private void transAddInt(int length, int x){
+        int start = 8 * (length - 1);
+        for(int i = 0; i < length; i++){
+            output.add((byte) (( x >> ( start - i * 8 )) & 0xFF ));
+        }
+    }
+
+    private void transAddLong(int length, long x){
         int start = 8 * (length-1);
         for(int i = 0 ; i < length; i++){
-            bytes.add((byte) (( target >> ( start - i * 8 )) & 0xFF ));
+            output.add((byte) (( x >> ( start - i * 8 )) & 0xFF ));
         }
-        return bytes;
-    }
-
-    private ArrayList<Byte> int2bytes(int length,int target){
-        ArrayList<Byte> bytes = new ArrayList<>();
-        int start = 8 * (length-1);
-        for(int i = 0 ; i < length; i++){
-            bytes.add((byte) (( target >> ( start - i * 8 )) & 0xFF ));
-        }
-        return bytes;
     }
 }
